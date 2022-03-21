3fdffd3cd9c7ba1ec0c05e9a2fb3bcbe6d996b48
felipeschirmann@gmail.com <felipeschirmann@gmail.com>
2022-03-20 23:59:25 -0300

    mapping JPA, exceptions and validations

--- src/main/java/com/devsuperior/movieflix/entities/Movie.java
+++ src/main/java/com/devsuperior/movieflix/entities/Movie.java
@@ -5,9 +5,22 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_movie")
 public class Movie implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String title;
 	private String subTitle;
@@ -15,8 +28,11 @@ public class Movie implements Serializable {
 	private String imgUrl;
 	private String synopsis;
 	
+	@ManyToOne
+	@JoinColumn(name = "genre_id")
 	private Genre genre = new Genre();
 	
+	@OneToMany(mappedBy = "movie")
 	private List<Review> reviews = new ArrayList<>();
 	
 	public Movie() {
